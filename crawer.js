4202d8ce1af02f49c98a7b99e41525cce53b9405
flykates <1062927638@qq.com>
2017-07-29 19:41:01 +0800

    http爬虫修改

--- crawer.js
+++ crawer.js
@@ -1,58 +1,57 @@
-/**
- * Created by weiqiujuan on 17-7-29.
- */
-var http = require('http');
-var cheerio = require('cheerio');
-var url = 'http://www.imooc.com/learn/328/';
+const http = require('http');
+const url = 'http://www.imooc.com/learn/348';
+const cheerio = require('cheerio');
 
-function filterChapters(html) {
+function filterChapters(html){
     var $ = cheerio.load(html);
 
-    var chapters = $('.learnchapter');
+    var chapters = $('.chapter');
 
-   var course = [];
-   chapters.each(function (item) {
-       var chapter = $(this);
-       var chapterTitle = chapter.find('strong').text();
-       var videos = chapter.find('.videos').children('li');
-       var chapterData = {
-           chapterTitle: chapterTitle,
-           video: []
-       };
-       videos.each(function (item) {
-           var video = $(this).find('.studyvideo');
-           var videoTitle = video.text();
-           var id = video.attr('href').split('video/')[1];
+    var courseDate = [];
+    chapters.each(function(item){
+        var chapter = $(this);
+        var chapterTitle = chapter.find('strong').text();
+        var videos = chapter.find('.video').find('li');
+        var chapterDate = {
+            chapterTitle : chapterTitle,
+            videos : []
+        }
+        videos.each(function(item){
+            var video = $(this).find('.J-media-item');
+            var videoTitle = video.text();
+            var videoId = video.attr('href').split('video/')[1];
 
-           chapterData.videos.push({
-               title: videoTitle,
-               id: id
-           })
-       });
-       courseData.push(chapterData);
-   });
-}
-function printCourseInfo(courseData) {
-  courseData.forEach(function (item) {
-      var chapterTitle = item.chapterTitle;
-      console.log(chapterTitle+'\n');
-      item.video.forEach(function (video){
-          console.log('   '+video.id+  video.title+ '\n')
-      } )
-  })
-}
-http.get(url,function (res) {
-    var html = '';
-
-    res.on('data',function (data) {
-        html += data
-    });
-
-    res.on('end',function () {
-        var courseData = filterChapters(html);
-
-        printCourseInfo(courseData);
-    }).on('error',function () {
-        console.log('获取页面数据错误')
+            chapterDate.videos.push({
+                title : videoTitle,
+                id : videoId
+            })
+        })
+        courseDate.push(chapterDate);
     })
+    return courseDate;
+}
+function printCourseDate(courseDate){
+    courseDate.forEach(function(item){
+        console.log(item.chapterTitle + '\n');
+        item.videos.forEach(function(video){
+            console.log('[' + video.id + ']' + video.title );
+        })
+    })
+}
+http.get(url,function(res){
+
+    var html='';
+    res.on('data',function(data){
+        html+=data;
+    })
+
+    res.on('end',function(){
+
+        var courseDate = filterChapters(html);
+        printCourseDate(courseDate);
+        // console.log(courseDate);
+
+    })
+}).on('error',function(){
+    console.log('出错了');
 });
